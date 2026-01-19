cab7a3dcd1887238049b523cf2def3bed22076be
Dmitry <dsikerin@gmail.com>
2026-01-20 01:35:12 +0700

    fix: add missing SetLocale middleware

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,20 @@
+﻿<?php
+# app/Http/Middleware/SetLocale.php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SetLocale
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $locale = session('locale') ?: config('app.locale', 'ru');
+
+        app()->setLocale($locale);
+
+        return $next($request);
+    }
+}
