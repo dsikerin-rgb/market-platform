d54b1edc0aa56cae4b84895cd3539e9968310305
Dmitry <dsikerin@gmail.com>
2026-03-19 18:00:53 +0700

    fix(tenants): surface with_debt as active has_debt filter chip

--- app/Filament/Resources/TenantResource/Pages/ListTenants.php
+++ app/Filament/Resources/TenantResource/Pages/ListTenants.php
@@ -19,6 +19,21 @@ class ListTenants extends ListRecords
         return 'Арендаторы';
     }
 
+    public function mount(): void
+    {
+        parent::mount();
+
+        if (! request()->boolean('with_debt')) {
+            return;
+        }
+
+        $currentValue = $this->tableFilters['has_debt']['value'] ?? null;
+
+        if (blank($currentValue)) {
+            $this->tableFilters['has_debt']['value'] = '1';
+        }
+    }
+
     protected function getHeaderActions(): array
     {
         return [
