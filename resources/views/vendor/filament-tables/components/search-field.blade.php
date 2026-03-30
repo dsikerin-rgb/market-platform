29f4499b95efae26d4b3f61581daa761668ed088
Dmitry <dsikerin@gmail.com>
2026-03-30 17:06:15 +0700

    Disable browser autofill in table search

---
+++ resources/views/vendor/filament-tables/components/search-field.blade.php
@@ -0,0 +1,55 @@
+@php
+    use Illuminate\View\ComponentAttributeBag;
+@endphp
+
+@props([
+    'debounce' => '500ms',
+    'onBlur' => false,
+    'placeholder' => __('filament-tables::table.fields.search.placeholder'),
+    'wireModel' => 'tableSearch',
+])
+
+@php
+    $wireModelAttribute = $onBlur ? 'wire:model.blur' : "wire:model.live.debounce.{$debounce}";
+@endphp
+
+<div
+    x-id="['input']"
+    {{ $attributes->class(['fi-ta-search-field']) }}
+>
+    <label x-bind:for="$id('input')" class="fi-sr-only">
+        {{ __('filament-tables::table.fields.search.label') }}
+    </label>
+
+    <x-filament::input.wrapper
+        inline-prefix
+        :prefix-icon="\Filament\Support\Icons\Heroicon::MagnifyingGlass"
+        :prefix-icon-alias="\Filament\Tables\View\TablesIconAlias::SEARCH_FIELD"
+        :wire:target="$wireModel"
+    >
+        <x-filament::input
+            :attributes="
+                (new ComponentAttributeBag)->merge([
+                    'autocomplete' => 'new-password',
+                    'autocapitalize' => 'off',
+                    'autocorrect' => 'off',
+                    'data-1p-ignore' => 'true',
+                    'data-form-type' => 'other',
+                    'data-lpignore' => 'true',
+                    'inlinePrefix' => true,
+                    'maxlength' => 1000,
+                    'name' => 'table-search-' . $this->getId() . '-' . $wireModel,
+                    'placeholder' => $placeholder,
+                    'readonly' => true,
+                    'spellcheck' => 'false',
+                    'type' => 'search',
+                    'wire:key' => $this->getId() . '.table.' . $wireModel . '.field.input',
+                    $wireModelAttribute => $wireModel,
+                    'x-bind:id' => '$id(\'input\')',
+                    'x-on:focus' => '$el.removeAttribute(\'readonly\')',
+                    'x-on:keyup' => 'if ($event.key === \'Enter\') { $wire.$refresh() }',
+                ], escape: false)
+            "
+        />
+    </x-filament::input.wrapper>
+</div>
