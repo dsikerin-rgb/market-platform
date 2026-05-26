6536a9f1ec232524d6c821ce4966a153bb2c508f
Dmitry <dsikerin@gmail.com>
2026-05-26 23:46:19 +0700

    Register market space exclusivity observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,8 +9,10 @@ use App\Http\Controllers\Admin\TenantDuplicateRestoreController;
 use App\Http\Controllers\Admin\TenantMergePreflightController;
 use App\Listeners\NotifySuperAdminsAboutUserLogin;
 use App\Models\IntegrationExchange;
+use App\Models\MarketSpace;
 use App\Models\Task;
 use App\Observers\IntegrationExchangeObserver;
+use App\Observers\MarketSpaceGroupSharedUseObserver;
 use App\Policies\TaskPolicy;
 use Filament\Http\Middleware\Authenticate as FilamentAuthenticate;
 use Filament\Support\Facades\FilamentView;
@@ -33,6 +35,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Gate::policy(Task::class, TaskPolicy::class);
         IntegrationExchange::observe(IntegrationExchangeObserver::class);
+        MarketSpace::observe(MarketSpaceGroupSharedUseObserver::class);
 
         Event::listen(Login::class, NotifySuperAdminsAboutUserLogin::class);
 
