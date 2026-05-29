0d9ccff3ed7bb668bf8934600a9b0c75ed8ea62f
Dmitry <dsikerin@gmail.com>
2026-05-29 18:24:59 +0700

    fix(review): remove unused review hook wrapper

--- resources/views/filament/partials/map-review-results-footer-hooks.blade.php
+++
@@ -1,4 +0,0 @@
-<div data-mrr-review-results-hooks style="display: contents">
-    @include('filament.partials.map-review-results-tab-controller')
-    @include('filament.partials.map-review-duplicate-space-picker')
-</div>
