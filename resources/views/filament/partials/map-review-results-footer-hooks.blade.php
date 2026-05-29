73460a1f04380d45e3298d8b38788c5da496cfd8
Dmitry <dsikerin@gmail.com>
2026-05-29 18:17:28 +0700

    fix(review): wrap review footer hook in blade root

---
+++ resources/views/filament/partials/map-review-results-footer-hooks.blade.php
@@ -0,0 +1,4 @@
+<div data-mrr-review-results-hooks style="display: contents">
+    @include('filament.partials.map-review-results-tab-controller')
+    @include('filament.partials.map-review-duplicate-space-picker')
+</div>
