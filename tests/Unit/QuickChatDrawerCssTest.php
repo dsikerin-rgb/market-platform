0cc2612d672dfe9d21f45d751e31851d48cf7309
Dmitry <dsikerin@gmail.com>
2026-07-02 11:15:25 +0700

    Fix quick chat input wrapping

--- tests/Unit/QuickChatDrawerCssTest.php
+++ tests/Unit/QuickChatDrawerCssTest.php
@@ -17,4 +17,18 @@ class QuickChatDrawerCssTest extends TestCase
         self::assertStringContainsString('overflow-y: auto;', $blade);
         self::assertStringNotContainsString("max-height: calc(100vh - 7rem);\n            overflow: auto;", $blade);
     }
+
+    public function test_message_composer_autogrows_after_livewire_updates(): void
+    {
+        $blade = file_get_contents(__DIR__.'/../../resources/views/livewire/admin/quick-chat-drawer.blade.php');
+
+        self::assertIsString($blade);
+        self::assertStringContainsString('box-sizing: border-box;', $blade);
+        self::assertStringContainsString('max-height: 10rem;', $blade);
+        self::assertStringContainsString('overflow-y: hidden;', $blade);
+        self::assertStringContainsString("this.\$el.style.height = 'auto';", $blade);
+        self::assertStringContainsString("this.\$el.style.overflowY = this.\$el.scrollHeight > maxHeight ? 'auto' : 'hidden';", $blade);
+        self::assertStringContainsString('x-effect="$wire.messageBody; $nextTick(() => resize())"', $blade);
+        self::assertStringContainsString('x-on:quick-chat-updated.window="$nextTick(() => resize())"', $blade);
+    }
 }
